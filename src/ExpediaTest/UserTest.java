808612f529f48348b06961942eb63d24a0afc826
JJGayso <gaysojj@rose-hulman.edu>
2015-04-16 22:21:13 -0400

    Updted TestDoubleMiles so that there is another flight booked by the target, at 700 miles (making the condition false, not being over 5000 miles), making MC/DC coverage at 100%.

--- src/ExpediaTest/UserTest.java
+++ src/ExpediaTest/UserTest.java
@@ -98,7 +98,21 @@ public class UserTest
 		Flight flight = new Flight(StartDate, EndDate, 10000);
 		target.bookWithDoubleMiles(new Booking[]{flight});
 		Assert.assertEquals(5000, target.bonusFrequentFlierMiles, 0.01);
+		
+		Flight otherFlight = new Flight(StartDate, EndDate, 700);
+		target.bookWithDoubleMiles(new Booking[]{otherFlight});
+		Assert.assertEquals((5000 + 1400), target.bonusFrequentFlierMiles, 0.01);
 	}
+	
+	@Test
+	public void TestThatDiscountInitializes()
+	{
+		Discount target = new Discount(0.01, 1);
+		ServiceLocator.Instance().AddDiscount(target);
+		this.target.book(new Booking[]{new Flight(StartDate, EndDate, 100), new Hotel(5), new Car(3)});
+		assertEquals(1024.65,this.target.Price(), 0.01);
+	}
+
 
 	
 	@After
